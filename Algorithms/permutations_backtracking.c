5aa47d0decc58aa683b83e93f9404482043937fa
Alexbm22 <alexandrub687@gmail.com>
2025-04-07 14:54:41 +0300

    Permutations with Backtracking

---
+++ Algorithms/permutations_backtracking.c
@@ -0,0 +1,76 @@
+// -- This algorithm should take a natural value and return all the permutations of that value. --
+// -- The algorithm should be implemented using backtracking. --
+
+#include <stdio.h>
+#define MAX_PERM_SIZE 25
+
+void init(int* v, int pos) {
+	v[pos] = 0;
+}
+
+int Succesor(int* v, int pos, int n) {
+	if (v[pos] < n) {
+		v[pos]++;
+		return 1;
+	}
+
+	return 0;
+}
+
+int	isValid(int* v, int pos) {
+	for (int i = 0; i <= pos; i++) {
+		for (int j = i + 1; j <= pos; j++) {
+			if (v[i] == v[j]) {
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+void PrintPermutation(int* v, int pos) {
+	for (int i = 0; i <= pos; i++) {
+		printf("%d", v[i]);
+	}
+
+	printf(" ");
+}
+
+void showPerms(int n) {
+	int pos = 0, perm[MAX_PERM_SIZE];
+	init(perm, pos);
+
+	while (pos >= 0) {
+		int isV = 0, isS = 0;
+		do {
+			isS = Succesor(perm, pos, n);
+			if (isS) {
+				isV = isValid(perm, pos);
+			}
+		} while (isS && !isV);
+
+		if (isS) {
+			if (pos == n - 1) {
+				PrintPermutation(perm, pos);
+			}
+			else {
+				init(perm, ++pos);
+			}
+		}
+		else {
+			init(perm, pos);
+			pos--;
+		}
+	}
+}
+
+int main() {
+
+	int n;
+	scanf("%d", &n);
+
+	printf("Permutations for %d numbers: ", n);
+	showPerms(n);
+
+	return 0;
+}
